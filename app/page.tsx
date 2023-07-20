59e0b21f712f2f58f31f428f6c024c12793ae7d9
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2023-07-20 15:55:54 +0200

    update image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Image
         src="https://evoluzione.fra1.cdn.digitaloceanspaces.com/media/schermata-2023-05-31-alle-20-02-52-1200x1200.png"
         alt="ciao"
-        //style={{ objectFit: "cover" }}
+        style={{ objectFit: "cover" }}
         fill
         className=''
       //sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw"
