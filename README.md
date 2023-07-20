65f240f7a54b5a2ec6c1d1f66eeae17da149c181
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2023-07-20 10:24:06 +0200

    add nextimage

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nextimage
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'evoluzione.fra1.cdn.digitaloceanspaces.com',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
