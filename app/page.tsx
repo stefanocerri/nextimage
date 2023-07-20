ded8329f66a584438c86221fc421bf62047baa67
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2023-07-20 15:30:49 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="{styles.container}">
+     <div className="{styles.container}">
       <Image
         src="https://evoluzione.fra1.cdn.digitaloceanspaces.com/media/schermata-2023-05-31-alle-20-02-52.png"
         alt="ciao"
